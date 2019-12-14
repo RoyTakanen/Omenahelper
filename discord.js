8e85fad1cfd5357214a9971c7b463ae38e202a0d
Kaikkitietokoneista.net <info@kaikkitietokoneista.net>
2019-12-14 12:54:04 +0200

    Update discord.js

--- discord.js
+++ discord.js
@@ -3,6 +3,9 @@ const moment = require('moment');
 const discord = require('discord.js');
 const client = new discord.Client();
 
+//voi sanoo !ruoka (insert koulu)
+
+
 var date = new Date();
 
 client.on('ready', () => {
@@ -10,36 +13,48 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content == "!ruoka") {
-    var month = date.getUTCMonth() + 1;
-    https.get('https://api.koulusafka.fi/get/index.php?a=' + date.getUTCFullYear() + '-' + month + '-' + date.getUTCDate() + '&b=fgfdg905hnm490jiofsdydy346gfgd&c=Aurinkolahden_peruskoulu&tylyurl=&_=1576054557009', (resp) => {
-    var formattedday = moment(date.getUTCFullYear() + '-' + month + '-' + date.getUTCDate());
-    var data = '';
+  if (msg.content == "!koulut") {
+    msg.channel.send("Helsingin koulut: https://kaikkitietokoneista.github.io/Omenahelper/index.html")
+  }
+  if (msg.content.startsWith("!ruoka")) {
+    var koulu = msg.content.substr("!ruoka ".length);
 
-        resp.on('data', (chunk) => {
-            data += chunk;
-        });
+    console.log(koulu);
 
-        resp.on('end', () => {
-            //bot.sendMessage(msg.chat.id, data);
-            try {
-                jsondata = JSON.parse(data);
+    if (koulu != "") {
+      var month = date.getUTCMonth() + 1;
+      https.get('https://api.koulusafka.fi/get/index.php?a=2019-11-13&b=fgfdg905hnm490jiofsdydy346gfgd&c=' + koulu + '&tylyurl=&_=1576054557009', (resp) => {
+      //https.get('https://api.koulusafka.fi/get/index.php?a=' + date.getUTCFullYear() + '-' + month + '-' + date.getUTCDate() + '&b=fgfdg905hnm490jiofsdydy346gfgd&c=' + koulu + '&tylyurl=&_=1576054557009', (resp) => {
+      var formattedday = moment(date.getUTCFullYear() + '-' + month + '-' + date.getUTCDate());
+      var data = '';
 
-                paivat = ["Ma:", "Ti:", "Ke:", "To:", "Pe:"];
+          resp.on('data', (chunk) => {
+              data += chunk;
+          });
 
-                var viesti = "Ruokana on (vko " + formattedday.week() + "):\n";
-                for (let nro = 0; nro < 5; nro++) {
-                    viesti += paivat[nro] + ' ' + jsondata.foods[nro].lunch + "\n";
-                }
-            } catch(error) {
-                console.log("Error " + error);
-                viesti = "Virheellinen päivämäärä. Tänään on " + formattedday.format("MMM Do YY"); + "\n";
-            }
+          resp.on('end', () => {
+              //bot.sendMessage(msg.chat.id, data);
+              try {
+                  jsondata = JSON.parse(data);
 
-            viesti += "\nTekijänä @Erikoisjääkäri#0939";
-            msg.reply(viesti);
-        });
-    });
+                  paivat = ["Ma:", "Ti:", "Ke:", "To:", "Pe:"];
+
+                  var viesti = "Ruokana on (vko " + formattedday.week() + "):\n";
+                  for (let nro = 0; nro < 5; nro++) {
+                      viesti += paivat[nro] + ' ' + jsondata.foods[nro].lunch + "\n";
+                  }
+              } catch(error) {
+                  console.log("Error " + error);
+                  viesti = "Virheellinen päivämäärä. Tänään on " + formattedday.format("MMM Do YY"); + "\n";
+              }
+
+              viesti += "\nTekijänä @Erikoisjääkäri#0939";
+              msg.reply(viesti);
+          });
+      });
+    } else {
+      msg.reply("Et kertonut koulua (saat koulut komennolla _!koulut_)");
+    }
   }
 });
 
